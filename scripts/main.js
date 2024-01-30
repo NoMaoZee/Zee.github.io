181a6cb5df03102b5bb9c22eec9bbf6284f9933f
NoMaoZee <zeedanmustamiargani@gmail.com>
2024-01-30 16:29:13 +0700

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
-//cClear form before after
+//Toggle & Responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+//Clear form before after
 window.onbeforeunload = () => {
     for(const form of document.getElementsByTagName("form")){
         form.reset();
     }
-}
+};
+    
